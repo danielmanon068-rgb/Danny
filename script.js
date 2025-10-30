8a006ec7532b534ec2d7ac3f0febc12aca11d69b
danielmanon068-rgb <danielmanon068@gmail.com>
2025-10-30 12:11:29 -0400

    Add files via upload

---
+++ script.js
@@ -0,0 +1,7 @@
+document.addEventListener("DOMContentLoaded", function () {
+  const btn = document.getElementById("btn-saludo");
+
+  btn.addEventListener("click", function () {
+    alert("¡Gracias por visitar nuestra página profesional!");
+  });
+});
